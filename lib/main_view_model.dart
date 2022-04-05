314fd41cbf06208d5d55ed4c0327798159bc545b
Oliver Zimmerman <oezimmerman@gmail.com>
2022-04-05 18:45:36 +0100

    [WEBRTC-1643] Use fake params

--- lib/main_view_model.dart
+++ lib/main_view_model.dart
@@ -106,13 +106,13 @@ class MainViewModel with ChangeNotifier {
   void call(String destination) {
     _telnyxClient
         .createCall()
-        .newInvite("Oliverz", "+353877189671", destination, "Fake State");
+        .newInvite("callerName", "Fake Number", destination, "Fake State");
   }
 
   void accept() {
     if (_incomingInvite != null) {
       _telnyxClient.createCall().acceptCall(
-          _incomingInvite!, "callerName", "+353877189671", "Fake State");
+          _incomingInvite!, "callerName", "Fake Number", "Fake State");
       _ongoingInvitation = false;
       _ongoingCall = true;
       notifyListeners();
