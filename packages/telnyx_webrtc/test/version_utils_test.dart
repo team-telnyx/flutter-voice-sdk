d7d0112a6e4a007b36de91e0e2ee74236e72588c
Oliver Zimmerman <oezimmerman@gmail.com>
2025-10-08 16:47:13 +0100

    fix: adjust tests to not look for a specific version that needs to be updated

--- packages/telnyx_webrtc/test/version_utils_test.dart
+++ packages/telnyx_webrtc/test/version_utils_test.dart
@@ -3,23 +3,38 @@ import 'package:telnyx_webrtc/utils/version_utils.dart';
 
 void main() {
   group('VersionUtils', () {
-    test('getSDKVersion returns constant version', () {
+    test('getSDKVersion returns valid semantic version', () {
       final version = VersionUtils.getSDKVersion();
-      expect(version, equals('3.0.1'));
+      expect(
+        version,
+        matches(
+          RegExp(r'^\d+\.\d+\.\d+$'),
+        ),
+      );
       expect(version, isNotEmpty);
     });
 
     test('getUserAgent returns properly formatted user agent string', () {
       final userAgent = VersionUtils.getUserAgent();
-      expect(userAgent, equals('Flutter-3.0.1'));
       expect(userAgent, startsWith('Flutter-'));
+      expect(
+        userAgent,
+        matches(
+          RegExp(r'^Flutter-\d+\.\d+\.\d+$'),
+        ),
+      );
     });
 
     test('getSDKVersion returns consistent result on multiple calls', () {
       final version1 = VersionUtils.getSDKVersion();
       final version2 = VersionUtils.getSDKVersion();
       expect(version1, equals(version2));
-      expect(version1, equals('3.0.1'));
+      expect(
+        version1,
+        matches(
+          RegExp(r'^\d+\.\d+\.\d+$'),
+        ),
+      );
     });
   });
 }
