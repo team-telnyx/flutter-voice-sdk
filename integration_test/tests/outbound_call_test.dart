f1aeb485a11878d636679e63541234c92b775744
Oliver Zimmerman <oezimmerman@gmail.com>
2026-02-10 11:55:37 +0000

    fix: use correct import for Timeout class

--- integration_test/tests/outbound_call_test.dart
+++ integration_test/tests/outbound_call_test.dart
@@ -1,4 +1,4 @@
-import 'package:test/test.dart';
+import 'package:test_api/src/backend/configuration/timeout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
